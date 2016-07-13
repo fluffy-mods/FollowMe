88e9b7f64947e77bfb8aa4eed6c767d8ff75acf1
FluffierThanThou <fluffy.l2032@gmail.com>
2016-07-13 16:30:05 +0200

    why detour no work?

---
+++ Source/ColonistBarDetours.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace FollowMe
+{
+    public class ColonistBarDetours
+    {
+        private float _clickedAt = Time.time;
+        private Pawn _clickedColonist;
+
+        private void HandleColonistClicks( Rect rect, Pawn colonist )
+        {
+            if ( !Mouse.IsOver( rect ) || Event.current.type != EventType.MouseDown )
+                return;
+            if ( _clickedColonist == colonist && (double)Time.time - (double)_clickedAt < 0.5 )
+            {
+                Event.current.Use();
+                JumpToTargetUtility.TryJump( (Thing)colonist );
+                _clickedColonist = (Pawn)null;
+            }
+            else
+            {
+                _clickedColonist = colonist;
+                _clickedAt = Time.time;
+            }
+            //if ( Mouse.IsOver( rect ) && Event.current.type == EventType.MouseDown )
+            //{
+            //    // double click jump (Vanilla)
+            //    if ( Event.current.button == 0 )
+            //    {
+            //        if ( _clickedColonist == colonist && Time.time - _clickedAt < 0.5 )
+            //        {
+            //            Event.current.Use();
+            //            JumpToTargetUtility.TryJump( colonist );
+            //            _clickedColonist = null;
+            //        }
+            //        else
+            //        {
+            //            _clickedColonist = colonist;
+            //            _clickedAt = Time.time;
+            //        }
+            //    }
+
+            //    // right-click follow
+            //    if ( Event.current.button == 1 )
+            //    {
+            //        Find.Selector.ClearSelection();
+            //        Find.Selector.Select( colonist );
+            //        FollowMe.TryStartFollow( colonist );
+            //    }
+            //}
+        }
+    }
+}
--- Source/FollowMe.cs
+++ Source/FollowMe.cs
@@ -1,13 +1,29 @@
 ﻿using RimWorld;
 using System;
 using System.Linq;
-using UnityEngine;
+using System.Reflection;
 using Verse;
+using CommunityCoreLibrary;
 
 namespace FollowMe
 {
     public class FollowMe : MapComponent
     {
+        public FollowMe()
+        {
+            // detour colonist bar click handler
+            MethodInfo SourceMethod = typeof( ColonistBar ).GetMethod( "HandleColonistClicks",
+                                                                       BindingFlags.NonPublic | BindingFlags.Instance );
+            MethodInfo DestinationMethod = typeof( ColonistBarDetours ).GetMethod( "HandleColonistClicks",
+                                                                       BindingFlags.NonPublic | BindingFlags.Instance );
+
+            if ( Detours.TryDetourFromTo( SourceMethod, DestinationMethod ) )
+                Log.Message( "FollowMe :: Succesfully injected right-click follow into Colonist Bar." );
+            else
+                Log.Error( "FollowMe :: Failed to inject right-click follow into Colonist Bar." );
+
+        }
+
         #region Fields
 
         private static bool _currentlyFollowing;
@@ -54,98 +70,15 @@ namespace FollowMe
 
             try
             {
-                // shut it off if we're manually scrolling (keys)
-                if ( _currentlyFollowing )
-                {
-                    if ( _followBreakingKeyBindingDefs.Any( key => key.IsDown ) )
-                    {
-                        Messages.Message( "FollowMe.Cancel".Translate( FollowedLabel ), MessageSound.Negative );
-                        _followedThing = null;
-                        _currentlyFollowing = false;
-                    }
-                }
-
                 // TODO: figure out how to shut it off when scrolling by mouse?
-
-                // get selection
-                Thing newFollowedThing = Find.Selector.SingleSelectedObject as Thing;
-
+                CheckFollowBreakingKeys();
+                
                 // start/stop following thing on key press
                 if ( _followKey.KeyDownEvent )
-                {
+                    TryStartFollow( Find.Selector.SingleSelectedObject as Thing );
 
-#if DEBUG
-                    Log.Message( "FollowMe :: Follow key pressed" );
-#endif
-
-                    // nothing to cancel or start following
-                    if ( !_currentlyFollowing && newFollowedThing == null )
-                    {
-                        if ( Find.Selector.NumSelected > 1 )
-                        {
-                            Messages.Message( "FollowMe.RejectMultiple".Translate(), MessageSound.RejectInput );
-                        }
-                        else if ( Find.Selector.NumSelected == 0 )
-                        {
-                            Messages.Message( "FollowMe.RejectNoSelection".Translate(), MessageSound.RejectInput );
-                        }
-                        else
-                        {
-                            Messages.Message( "FollowMe.RejectNotAThing".Translate(), MessageSound.RejectInput );
-                        }
-                    }
-
-                    // cancel current follow
-                    else if ( _currentlyFollowing && newFollowedThing == null || newFollowedThing == _followedThing )
-                    {
-                        Messages.Message( "FollowMe.Cancel".Translate( FollowedLabel ), MessageSound.Negative );
-                        _followedThing = null;
-                        _currentlyFollowing = false;
-                    }
-
-                    // follow new thing
-                    else if ( newFollowedThing != null )
-                    {
-                        _followedThing = newFollowedThing;
-                        _currentlyFollowing = true;
-                        Messages.Message( "FollowMe.Follow".Translate( FollowedLabel ), MessageSound.Negative );
-                    }
-                }
-
-                // try follow whatever thing is selected
-                if ( _currentlyFollowing && _followedThing != null )
-                {
-                    if ( !_followedThing.Spawned && _followedThing.holder != null )
-                    {
-                        // thing is in some sort of container
-                        IThingContainerOwner holder = _followedThing.holder.owner;
-
-                        // if holder is a pawn's carrytracker we can use the smoother positions of the pawns's drawposition
-                        Pawn_CarryTracker tracker = holder as Pawn_CarryTracker;
-                        if ( tracker != null )
-                        {
-                            Find.CameraDriver.JumpTo( tracker.pawn.DrawPos );
-                        }
-
-                        // otherwise the holder int location will have to do
-                        else
-                        {
-                            Find.CameraDriver.JumpTo( holder.GetPosition() );
-                        }
-                    }
-                    else if ( _followedThing.Spawned )
-                    {
-                        // thing is spawned in world, just use the things drawPos
-                        Find.CameraDriver.JumpTo( _followedThing.DrawPos );
-                    }
-                    else
-                    {
-                        // we've lost track of whatever it was we were following
-                        Log.Message( "FollowMe.Cancel".Translate( FollowedLabel ) );
-                        _currentlyFollowing = false;
-                        _followedThing = null;
-                    }
-                }
+                // move camera
+                Follow();
             }
 
             // catch exception to avoid error spam
@@ -154,6 +87,80 @@ namespace FollowMe
                 _enabled = false;
                 Log.Error( e.ToString() );
             }
+        }
+
+        private static void Follow()
+        {
+            if ( !_currentlyFollowing || _followedThing == null )
+                return;
+
+            if ( !_followedThing.Spawned && _followedThing.holder != null )
+            {
+                // thing is in some sort of container
+                IThingContainerOwner holder = _followedThing.holder.owner;
+
+                // if holder is a pawn's carrytracker we can use the smoother positions of the pawns's drawposition
+                Pawn_CarryTracker tracker = holder as Pawn_CarryTracker;
+                if ( tracker != null )
+                    Find.CameraDriver.JumpTo( tracker.pawn.DrawPos );
+
+                // otherwise the holder int location will have to do
+                else
+                    Find.CameraDriver.JumpTo( holder.GetPosition() );
+            }
+
+            // thing is spawned in world, just use the things drawPos
+            else if ( _followedThing.Spawned )
+                Find.CameraDriver.JumpTo( _followedThing.DrawPos );
+
+            // we've lost track of whatever it was we were following
+            else
+                StopFollow();
+        }
+
+        public static void TryStartFollow( Thing thing )
+        { 
+            if ( !_currentlyFollowing && thing == null )
+            {
+                if ( Find.Selector.NumSelected > 1 )
+                    Messages.Message( "FollowMe.RejectMultiple".Translate(), MessageSound.RejectInput );
+
+                else if ( Find.Selector.NumSelected == 0 )
+                    Messages.Message( "FollowMe.RejectNoSelection".Translate(), MessageSound.RejectInput );
+
+                else
+                    Messages.Message( "FollowMe.RejectNotAThing".Translate(), MessageSound.RejectInput );
+            }
+
+            // cancel current follow (toggle or thing == null)
+            else if ( _currentlyFollowing && thing == null || thing == _followedThing )
+                StopFollow();
+
+            // follow new thing
+            else if ( thing != null )
+                StartFollow( thing );
+        }
+
+        private static void StartFollow( Thing thing )
+        {
+            _followedThing = thing;
+            _currentlyFollowing = true;
+            Messages.Message( "FollowMe.Follow".Translate( FollowedLabel ), MessageSound.Negative );
+        }
+
+        public static void StopFollow()
+        {
+            Messages.Message( "FollowMe.Cancel".Translate( FollowedLabel ), MessageSound.Negative );
+            _followedThing = null;
+            _currentlyFollowing = false;
+        }
+
+        private void CheckFollowBreakingKeys()
+        {
+            if ( !_currentlyFollowing )
+                return;
+            if ( _followBreakingKeyBindingDefs.Any( key => key.IsDown ) )
+                StopFollow();
         }
     }
 }
