e802bcce581afe6bfc64362d4e0e45a0e4a72f5a
FluffierThanThou <fluffy.l2032@gmail.com>
2017-05-21 23:41:31 +0200

    'Release v0.17.0.3 [nolog]'

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.17.0.2")]
-[assembly: AssemblyFileVersion("0.17.0.2")]
+[assembly: AssemblyVersion("0.17.0.3")]
+[assembly: AssemblyFileVersion("0.17.0.3")]
