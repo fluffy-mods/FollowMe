3d24cf7c35a5d9c37bca81d4dcfd40d17737ee73
Fluffy <fluffy.l2032@gmail.com>
2017-11-25 14:58:08 +0100

    'Release v0.18.2.0 [nolog]'

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("18.1.2.9")]
-[assembly: AssemblyFileVersion("18.1.2.9")]
+[assembly: AssemblyVersion("18.2.0.10")]
+[assembly: AssemblyFileVersion("18.2.0.10")]
