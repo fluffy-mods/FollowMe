20859ca2420ba380dea9bec6e21051a11e07a70b
Fluffy <fluffy.l2032@gmail.com>
2018-10-02 10:14:34 +0200

    'Release 2.10.35 [nolog]'

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -34,5 +34,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.9.33")]
-[assembly: AssemblyFileVersion("2.9.33")]
+[assembly: AssemblyVersion("2.10.35")]
+[assembly: AssemblyFileVersion("2.10.35")]
